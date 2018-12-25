bf199c1ca0eb47c9df902dc9ebeab82ae7dfe5b7
José Sánchez <joseasv@outlook.com>
2018-12-25 16:24:19 -0400

    volviendo a cambiar el script

--- bin/deploy.ps1
+++ bin/deploy.ps1
@@ -7,7 +7,8 @@ git checkout -b gh-pages
 bundle exec jekyll build
 
 # Delete and move files
-Remove-Item * -Recurse -Exclude _site 
+Get-ChildItem -Path $dir -Exclude _site |
+ForEach-Object {Remove-Item $_ -Recurse }
 Copy-Item _site/*.* pwd
 Remove-Item _site -Recurse
 
