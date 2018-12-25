5b8141dd75413b52494198810bb35695e28628c4
José Sánchez <joseasv@outlook.com>
2018-12-25 16:05:19 -0400

    cambiando de nuevo el script

---
+++ bin/deploy.ps1
@@ -0,0 +1,20 @@
+
+git branch -D gh-pages
+
+git checkout -b gh-pages
+
+# Build site
+bundle exec jekyll build
+
+# Delete and move files
+Remove-Item * -Recurse -Exclude _site 
+Copy-Item _site .
+Remove-Item _site
+
+# Push to DEPLOY_BRANCH
+git add -fA
+git commit --allow-empty -m "$(git log -1 --pretty=%B) [ci skip]"
+git push -f -q origin gh-pages
+
+# Move back to SRC_BRANCH
+git checkout master
